294bd43ab57a5c78dd4f978b9e126cd57e53dac4
Evgen Polivanov <polivanov004@gmail.com>
2025-01-29 17:06:01 +0300

    brightness

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,17 +36,25 @@ android {
 }
 
 dependencies {
-
+    //map
+    implementation(libs.osmdroid.android)
+    implementation("androidx.preference:preference:1.2.1")
+    //location
     implementation(libs.play.services.location)
-
+    //supabase
     implementation("io.github.jan-tennert.supabase:postgrest-kt:3.0.0")
     implementation("io.github.jan-tennert.supabase:auth-kt:3.0.0")
     implementation("io.ktor:ktor-client-okhttp:3.0.0")
+    //default
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    //camera
+    implementation(libs.androidx.camera.lifecycle)
+    implementation(libs.androidx.camera.view)
+    //tests
     testImplementation(libs.junit)
     testImplementation(libs.kotlinx.coroutines.test)
     androidTestImplementation(libs.androidx.junit)
--- app/src/main/java/com/example/swipeloacationcamera/MainActivity.kt
+++ app/src/main/java/com/example/swipeloacationcamera/MainActivity.kt
@@ -4,7 +4,6 @@ import android.Manifest
 import android.annotation.SuppressLint
 import android.content.pm.PackageManager
 import android.location.Geocoder
-import android.location.Location
 import android.location.LocationManager
 import android.os.Build
 import android.os.Bundle
@@ -18,6 +17,8 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.view.PreviewView
 import androidx.cardview.widget.CardView
 import androidx.core.app.ActivityCompat
 import androidx.lifecycle.MutableLiveData
@@ -27,46 +28,72 @@ import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationCallback
 import com.google.android.gms.location.LocationResult
 import com.google.android.gms.location.LocationServices
+import com.google.common.util.concurrent.ListenableFuture
 import io.github.jan.supabase.auth.auth
 import io.github.jan.supabase.auth.exception.AuthRestException
 import io.github.jan.supabase.auth.providers.builtin.Email
 import io.github.jan.supabase.exceptions.HttpRequestException
-import kotlinx.coroutines.awaitAll
 import kotlinx.coroutines.launch
+import org.osmdroid.config.Configuration.getInstance
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory
+import org.osmdroid.util.GeoPoint
+import org.osmdroid.views.MapView
+import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider
+import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay
 import java.util.Locale
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var cameraProviderFeature: ListenableFuture<ProcessCameraProvider>
     private lateinit var fusedLocationClient: FusedLocationProviderClient
     private lateinit var locationCallback: LocationCallback
     private val address = MutableLiveData<String>()
     private var locationManager : LocationManager? = null
+    private lateinit var previewView:PreviewView
+    private lateinit var map: MapView
+    private lateinit var mLocationOverlay: MyLocationNewOverlay
 
     @SuppressLint("ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        getInstance().load(this, androidx.preference.PreferenceManager.getDefaultSharedPreferences(this))
         setContentView(R.layout.activity_main)
+
+        //Затемнение экрана
+        window.attributes = window.attributes.apply {
+            screenBrightness = 0f
+        }
+        map = findViewById(R.id.map)
+        map.setTileSource(TileSourceFactory.MAPNIK)
+
+
+
+        //Запрос к БД на авторизацию
         lifecycleScope.launch {
             try {
                 SupabaseConn.supabase.auth.clearSession()
                 SupabaseConn.supabase.auth.signInWith(Email){
-                    email = "testuse@gmail.com"
+                    email = "testuser@gmail.com"
                     password = "User12345678!"
                 }
             }catch (ex : AuthRestException){
+                //Сообщение когда неверные данные
                 Log.e("AuthFailed", ex.message.toString())
                 Log.e("AuthFailed", ex.statusCode.toString())
                 Log.e("AuthFailed", ex.description.toString())
             }catch (ex : HttpRequestException){
+                //Разраыв соединения
                 Log.e("InternetConnection", ex.message.toString())
             }
             catch (ex : Exception){
+                //Другие ошибки
                 Log.e("auth", ex.message.toString())
                 Log.e("auth", ex.cause.toString())
                 Log.e("auth", ex.toString())
             }
 
         }.invokeOnCompletion {
+            //Вывод результата когда запрос выполнен без ошибок
             Log.e("supabaseResult", SupabaseConn.supabase.auth.currentUserOrNull().toString())
         }
 
@@ -75,6 +102,7 @@ class MainActivity : AppCompatActivity() {
         val rightCard = findViewById<CardView>(R.id.redCard)
         val btnGetLocation = findViewById<Button>(R.id.btnGetLocation)
         val addressTextView = findViewById<TextView>(R.id.addressTextView)
+        //Получение геопозиции
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
         locationManager = getSystemService(LOCATION_SERVICE) as LocationManager?
         address.observe(this){
@@ -87,34 +115,19 @@ class MainActivity : AppCompatActivity() {
                 }
             }
         }
-
+        //Обработка свайпа по карточке товара
         val gestureDetector = GestureDetector(this, object : GestureDetector.OnGestureListener{
             private val swipeThreshold = 100
-            override fun onDown(p0: MotionEvent): Boolean {
-                return true
-            }
-
-            override fun onShowPress(p0: MotionEvent) {
-
-            }
-
-            override fun onSingleTapUp(p0: MotionEvent): Boolean {
-                return true
-            }
-
+            override fun onDown(p0: MotionEvent): Boolean = true
+            override fun onShowPress(p0: MotionEvent) {}
+            override fun onSingleTapUp(p0: MotionEvent): Boolean = true
             override fun onScroll(
                 p0: MotionEvent?,
                 p1: MotionEvent,
                 p2: Float,
                 p3: Float
-            ): Boolean {
-                return true
-            }
-
-            override fun onLongPress(p0: MotionEvent) {
-
-            }
-
+            ): Boolean = true
+            override fun onLongPress(p0: MotionEvent) {}
             override fun onFling(
                 e1: MotionEvent?,
                 e2: MotionEvent,
@@ -134,71 +147,68 @@ class MainActivity : AppCompatActivity() {
 
             }
         })
+        mainCardView.setOnTouchListener { _, event -> gestureDetector.onTouchEvent(event) }
 
 
-
+        //Запрос на доступ к геолокации
         val locationPermissionRequest = registerForActivityResult(
             ActivityResultContracts.RequestMultiplePermissions()
         ) { permissions ->
             when {
                 permissions.getOrDefault(android.Manifest.permission.ACCESS_FINE_LOCATION, false) -> {
-                    getCurrentLocation()
+                    getAddress()
                 }
                 permissions.getOrDefault(android.Manifest.permission.ACCESS_COARSE_LOCATION, false) -> {
-                    getCurrentLocation()
+                    getAddress()
                 } else -> {
                     Toast.makeText(this, "Access denide", Toast.LENGTH_LONG).show()
                 }
             }
         }
-        mainCardView.setOnTouchListener { _, event -> gestureDetector.onTouchEvent(event) }
+
+
         btnGetLocation.setOnClickListener {
             try {
+
                 locationPermissionRequest.launch(arrayOf(
                     android.Manifest.permission.ACCESS_FINE_LOCATION,
                     android.Manifest.permission.ACCESS_COARSE_LOCATION))
             }catch (ex : Exception){
 
             }
-
         }
-
-
     }
 
-    private fun getCurrentLocation() {
-        if (ActivityCompat.checkSelfPermission(
-                this,
-                Manifest.permission.ACCESS_FINE_LOCATION
-            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
-                this,
-                Manifest.permission.ACCESS_COARSE_LOCATION
-            ) != PackageManager.PERMISSION_GRANTED
-        ) {
-            return
-        }
-        locationManager?.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0L, 0f
-        ) {
-            getAddress(it)
-        }
-//        fusedLocationClient.lastLocation
-//            .addOnSuccessListener { location ->
-//                getAddress(location)
-//            }
-    }
 
-    private fun getAddress(location: Location) {
-        Log.e("Location", location.toString())
-        val geo = Geocoder(this, Locale("RU-ru"))
+    private fun getAddress() {
+        mLocationOverlay = MyLocationNewOverlay(GpsMyLocationProvider(this), map)
+        mLocationOverlay.enableMyLocation()
+        map.overlays.add(mLocationOverlay)
+        map.invalidate()
+        try {
+            val latitude = mLocationOverlay.myLocation.latitude
+            val longitude = mLocationOverlay.myLocation.longitude
+            //Перемещение к точке
+            map.controller.setCenter(GeoPoint(latitude, longitude))
+            map.setZoomLevel(13.0)
+            //Получение текущей позиции
+            //Log.e("Location", location.toString())
+            val geo = Geocoder(this, Locale("RU-ru"))
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-            geo.getFromLocation(location.latitude, location.longitude, 1) {
-                Log.e("loc", it.toString())
-                address.postValue(it.first().getAddressLine(0))
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                geo.getFromLocation(latitude, longitude, 1) {
+                    Log.e("loc", it.toString())
+                    address.postValue(it.first().getAddressLine(0))
+                }
+            } else {
+                address.postValue("VERSION CODE less than TIRAMISU")
             }
-        } else {
-            address.postValue("VERSION CODE less than TIRAMISU")
+        }catch (ex : Exception){
+
         }
+
     }
+
+
 
 }
